90a4d2a4c1a089498e6ce6f185feae3a6d13c110
haydenmorgan1999 <hmloeb@ncsu.edu>
2025-06-29 16:29:44 -0400

    commit 3/5, sets up the first tab of the app

--- project2/app.R
+++ project2/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library("httr")
 library("jsonlite")
 library("tidyverse")
@@ -21,8 +12,12 @@ ui <- page_fluid(
               p("The data comes from the Hyrule Compendium API, which can be found", 
                 a("here", href = "https://gadhagod.github.io/Hyrule-Compendium-API/#/", target = "_blank")), 
               p("The current tab is the 'About' tab, which explains the app. The 'Data Download' tab allows you to query data and save your query. The 'Data Exploration' tab allows you to customize and display numerical/graphical summaries of the data."),
-              img(src = , height = "300px")
-    )
+              img(src = "picture.jpg", height = "300px")
+    ),
+    
+    nav_panel("Data Download",),
+    
+    nav_panel("Data Exploration",)
   )
 )
 

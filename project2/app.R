edbd53accf9db746f5b5b8b3ffcf77b811a57466
haydenmorgan1999 <hmloeb@ncsu.edu>
2025-06-29 16:21:02 -0400

    commit 2/5: first commit was most of "find data you are interested in". This commit sets up the initial app space.

---
+++ project2/app.R
@@ -0,0 +1,33 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library("httr")
+library("jsonlite")
+library("tidyverse")
+library("shiny")
+library("bslib")
+
+#UI
+ui <- page_fluid(
+  navset_card_underline(
+    nav_panel("About", 
+              p("The purpose of this app is to search the Hyrule Compendium and display its data in various ways."), 
+              p("The data comes from the Hyrule Compendium API, which can be found", 
+                a("here", href = "https://gadhagod.github.io/Hyrule-Compendium-API/#/", target = "_blank")), 
+              p("The current tab is the 'About' tab, which explains the app. The 'Data Download' tab allows you to query data and save your query. The 'Data Exploration' tab allows you to customize and display numerical/graphical summaries of the data."),
+              img(src = , height = "300px")
+    )
+  )
+)
+
+#Server
+server <- 
+  
+  #Run app 
+  shinyApp(ui = ui, server = server)
